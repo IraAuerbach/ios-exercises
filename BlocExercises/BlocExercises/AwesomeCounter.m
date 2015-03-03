cdfec7eb2a9e523fbe4cb4c13f7141fedf388caa
Ira Auerbach <auerbach.ira@gmail.com>
2015-03-03 10:42:59 -0500

    Completed Loops

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,30 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    
+    NSString *output = @"";
+    
+    //Boolean tests to organize direction of loop
+    BOOL isEqual = (number - otherNumber == 0) ? YES : NO;
+    BOOL numberIsHigher = (number - otherNumber > 0) ? YES : NO;
+    BOOL otherNumberIsHigher = (number - otherNumber < 0) ? YES : NO;
+    
+    //conditional statements based on organization booleans above
+    if (isEqual) {
+        output = [NSString stringWithFormat:@"%ld", number];
+    }
+    else if(numberIsHigher){
+        for(NSInteger counter = 0; counter <= (number-otherNumber); counter++){
+            output = [NSString stringWithFormat:@"%@%ld", output, otherNumber+counter];
+        }
+    }
+    else if(otherNumberIsHigher){
+        for (NSInteger counter = 0; counter <= (otherNumber-number); counter++) {
+            output = [NSString stringWithFormat:@"%@%ld", output, number+counter];
+        }
+    }
+    
+    return output;
 }
 
 @end
