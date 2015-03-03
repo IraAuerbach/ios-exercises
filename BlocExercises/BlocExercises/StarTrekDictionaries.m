1ccf1d377c4a8565f5f372a5f55b5ea439141f70
Ira Auerbach <auerbach.ira@gmail.com>
2015-03-03 13:59:34 -0500

    Completed Dictionaries

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -12,17 +12,39 @@
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
+    id favoriteDrink = characterDictionary[@"favorite drink"];
+    
+    if (favoriteDrink != nil && [favoriteDrink isKindOfClass:[NSString class]]){
+        return favoriteDrink;
+    }
+    
     return @"";
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
     /* WORK HERE */
-    return @[];
+    
+    //I used an NSMutableArray instead of NSArray, not sure if that is cheating but I couldnt figure out any other way to do it
+    NSMutableArray *favoriteDrinkArray = [[NSMutableArray alloc] init];
+    
+    for(NSInteger index = 0; index < charactersArray.count; index++){
+        if (charactersArray[index] != nil && [charactersArray[index] isKindOfClass:[NSDictionary class]]) {
+            NSDictionary *characterDetails = charactersArray[index];
+            NSString *favoriteDrinks = characterDetails[@"favorite drink"];
+            [favoriteDrinkArray addObject:favoriteDrinks];
+        }
+    }
+    
+    return favoriteDrinkArray;
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @{};
+    
+    NSMutableDictionary *characterDictionaryWithQuoteAdded = [characterDictionary mutableCopy];
+    [characterDictionaryWithQuoteAdded setObject:@"I have a dream!" forKey:@"quote"];
+    
+    return characterDictionaryWithQuoteAdded;
 }
 
 @end
